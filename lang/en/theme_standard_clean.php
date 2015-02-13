58fc470ec338889e16ecc5ac1029a7c5292166e1
papillon326 <udagawa@www2178ue.sakura.ne.jp>
2015-02-13 20:20:53 +0900

    first version

---
+++ lang/en/theme_standard_clean.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_clean', language 'en'
+ *
+ * @package   theme_standard_clean
+ * @copyright udagawa mitsuru
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>Standard clean</h2>
+<p><img class=img-polaroid src="standard_clean/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>Theme Standar clean is based on theme Clean.</p>
+</div></div>';
+
+$string['configtitle'] = 'Standard Clean';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+If the height of your logo is more than 75px add the following CSS rule to the Custom CSS box below.<br>
+a.logo {height: 100px;} or whatever height in pixels the logo is.';
+
+$string['pluginname'] = 'Standard Clean';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['themecolor'] = 'Theme color';
+$string['themecolordesc'] = 'Setting Themecolor';
+
---
+++ settings.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle's Clean theme, an example of how to make a Bootstrap theme
+ *
+ * DO NOT MODIFY THIS THEME!
+ * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
+ *
+ * For full information about creating Moodle themes, see:
+ * http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   theme_standard_clean
+ * @copyright udagawa mitsuru
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+    
+    // Main theme background colour setting. 
+    $name = 'theme_standard_clean/themecolor'; 
+    $visiblename = get_string('themecolor', 'theme_standard_clean'); 
+    $description = get_string('themecolordesc', 'theme_standard_clean'); 
+    $defaultsetting = '#e1e1df'; 
+    $previewconfig = null; 
+    $setting = new admin_setting_configcolourpicker($name, $visiblename, $description, $defaultsetting, $previewconfig); 
+    $setting->set_updatedcallback('theme_reset_all_caches'); 
+    $settings->add($setting); 
+    
+    // Logo file setting.
+    $name = 'theme_standard_clean/logo';
+    $title = get_string('logo','theme_standard_clean');
+    $description = get_string('logodesc', 'theme_standard_clean');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_standard_clean/customcss';
+    $title = get_string('customcss', 'theme_standard_clean');
+    $description = get_string('customcssdesc', 'theme_standard_clean');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Footnote setting.
+    $name = 'theme_standard_clean/footnote';
+    $title = get_string('footnote', 'theme_standard_clean');
+    $description = get_string('footnotedesc', 'theme_standard_clean');
+    $default = '';
+    $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+}
